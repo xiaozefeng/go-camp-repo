961185506e00a433449a00f77d4525f056e4c6dc
xiaozefeng <qq523107430@163.com>
2021-09-11 01:17:08 +0800

    feat(location): week2 homework

--- homework/week2/handler_sql_no_row_error.go
+++ homework/week2/handler_sql_no_row_error.go
@@ -1,12 +1,3 @@
-##  我们在数据库操作的时候，比如 dao 层中当遇到一个 sql.ErrNoRows 的时候，是否应该 Wrap 这个 error，抛给上层。为什么，应该怎么做请写出代码？
-
-sql.ErrNoRows 这个error 表示没有数据
-在真实的场景中，sql查不到数据是很正常的，这个error应该在底层就被处理，直接返回空数据给上层，而不需要返回error
-所以我的结论是：不应该wrap这个error抛给上层，
-
-**代码**
-
-```go
 package week2
 
 import (
@@ -47,5 +38,3 @@ func ListPersontByName(name string) ([]*Person, error) {
 func IsNoRowsErr(err error) bool {
 	return errors.Is(err, sql.ErrNoRows)
 }
-```
-
